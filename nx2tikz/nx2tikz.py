2d38435b3649022f23ef1a976907fd175486c87d
Ioannis Filippidis <jfilippidis@gmail.com>
2015-08-13 23:37:31 -0700

    pep8

--- nx2tikz/nx2tikz.py
+++ nx2tikz/nx2tikz.py
@@ -1,39 +1,31 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""
-Export NetworkX graphs to TikZ graphs with automatic layout
-
-Copyright 2014 Ioannis Filippidis, California Institute of Technology
-"""
+"""Export NetworkX graphs to TikZ graphs with automatic layout."""
 import argparse
 import subprocess
 from networkx import is_directed, Graph
 
+
 def dumps_tikz(g, preamble=True, layout='layered', use_label=True):
-    if layout not in {'layered','spring'}:
-        raise ValueError('Unknown layout: ' + str(layout))
-    
+    if layout not in {'layered', 'spring'}:
+        raise ValueError('Unknown layout: {s}'.format(s=layout))
     s = ''
     for n, d in g.nodes_iter(data=True):
+        # label
         label = g.node[n].get('label', '')
         label = 'as={' + label + '}' if label else ''
-        
+        # geometry
         color = d.get('color', '')
         fill = d.get('fill', '')
         shape = d.get('shape', '')
-        
+        # style
         style = ', '.join(filter(None, [label, color, fill, shape]))
         style = '[' + style + ']' if style else ''
-        
+        # pack them
         s += str(n) + style + ';\n'
-    
     s += '\n'
-    
     if is_directed(g):
         line = ' -> '
     else:
         line = ' -- '
-    
     for u, v, d in g.edges_iter(data=True):
         if use_label:
             label = d.get('label', '')
@@ -41,31 +33,23 @@ def dumps_tikz(g, preamble=True, layout='layered', use_label=True):
         else:
             label = str(d)
             color = ''
-        
         if label:
             label = '"' + label + '"\' above'
-        
         loop = 'loop' if u is v else ''
-        
         style = ', '.join(filter(None, [label, color, loop]))
         style = ' [' + style + '] ' if style else ''
-        
         s += str(u) + line + style + str(v) + ';\n'
-    
     tikzpicture = (
         r'\begin{tikzpicture}' '\n'
         '\graph[' + layout + ' layout, sibling distance=5.0cm,'
-        #'edge quotes mid,'
+        # 'edge quotes mid,'
         'edges={nodes={sloped, inner sep=10pt}},'
         'nodes={circle,draw}]{\n' +
         s +
         '};\n'
-        '\end{tikzpicture}\n'
-    )
-    
+        '\end{tikzpicture}\n')
     if not preamble:
         return tikzpicture
-    
     if layout == 'layered':
         layout_lib = 'layered'
     elif layout == 'spring':
@@ -73,7 +57,6 @@ def dumps_tikz(g, preamble=True, layout='layered', use_label=True):
     else:
         raise ValueError('Unknown which library includes the layout: ' +
                          str(layout))
-    
     document = (
         '\documentclass{minimal}\n'
         '\usepackage{amsmath}\n'
@@ -85,31 +68,33 @@ def dumps_tikz(g, preamble=True, layout='layered', use_label=True):
         r'\begin{document}' '\n'
         '\n' +
         tikzpicture +
-        '\end{document}\n'
-    )
+        '\end{document}\n')
     return document
+
 
 def write_tikz(g, fname, preamble=True):
     f = open(fname, 'w')
     f.write(dumps_tikz(g))
     f.close()
 
+
 def write_pdf(g, fname, use_label=True):
     opt = ['lualatex', '--jobname', fname]
     p = subprocess.Popen(opt, stdin=subprocess.PIPE)
-    
-    p.stdin.write(dumps_tikz(g, preamble=True, use_label=use_label))
+    s = dumps_tikz(g, preamble=True, use_label=use_label)
+    p.stdin.write(s)
     p.stdin.close()
     p.wait()
 
 if __name__ == '__main__':
     g = Graph()
+    # nodes
     g.add_node(1, label='$a$', color='yellow', shape='ellipse')
     g.add_node(2, label='$b$', color='blue', fill='orange', shape='circle')
     g.add_node(3, label='$c$', shape='rectangle')
     g.add_node(4, label='$E=mc^2$')
     g.add_node(5, label=r'$\begin{bmatrix} x_1\\ x_2\\ x_3\end{bmatrix}$')
-    
+    # edges
     g.add_edge(1, 2, label='$\{p\}$')
     g.add_edge(1, 3, label='$\{a,b\}$', color='purple')
     g.add_edge(3, 4, label=r'$\begin{matrix} x=1\\ y=2\\ z=10 \end{matrix}$')
@@ -117,19 +102,15 @@ if __name__ == '__main__':
     g.add_edge(5, 4, color='red')
     g.add_edge(2, 2, color='blue')
     g.add_edge(4, 1)
-    
-    desc = 'Convert networkx graphs to tikz.'
-    
+    desc = 'Convert a NetworkX graph to TikZ.'
     parser = argparse.ArgumentParser(prog='nx2tikz', description=desc)
     parser.add_argument('--format', '-f', help='output format',
                         choices={'pdf', 'tikz'}, default='pdf')
-    
     opts = parser.parse_args()
-    
     if opts.format == 'pdf':
         write_pdf(g, 'hehe')
     elif opts.format == 'tikz':
         s = dumps_tikz(g)
         print(s)
     else:
-        raise Exception('Unknow output format: ' + opts.format)
+        raise Exception('Unknow output format: {f}'.format(f=opts.format))
--- setup.py
+++ setup.py
@@ -1,28 +1,28 @@
 # -*- coding: utf-8 -*-
-
 from setuptools import setup
 import warnings
 
+
 README = 'README.md'
 name = 'nx2tikz'
-
 try:
     long_description = open('README.md').read()
 except:
     warnings.warn('Could not find {readme}'.format(readme=README))
 
-setup(
-    name=name,
-    version='0.1',
-    license='BSD',
-    description='Export NetworkX graphs to TikZ.',
-    long_description=long_description,
-    author='Ioannis Filippidis',
-    author_email='jfilippidis@gmail.com',
-    url = 'https://github.com/johnyf/nx2tikz',
-    install_requires=['networkx'],
-    packages=[name],
-    package_dir={name: name},
-    keywords = ['tikz', 'pgf', 'networkx', 'tex', 'latex', 'graph'],
-    classifiers = [],
-)
+
+if __name__ == '__main__':
+    setup(
+        name=name,
+        version='0.1',
+        license='BSD',
+        description='Export NetworkX graphs to TikZ.',
+        long_description=long_description,
+        author='Ioannis Filippidis',
+        author_email='jfilippidis@gmail.com',
+        url='https://github.com/johnyf/nx2tikz',
+        install_requires=['networkx'],
+        packages=[name],
+        package_dir={name: name},
+        keywords=['tikz', 'pgf', 'networkx', 'tex', 'latex', 'graph'],
+        classifiers=[])
