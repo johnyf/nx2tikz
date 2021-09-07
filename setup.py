3d335c1547dd389499e46f684774bdb427b0a84b
Ioannis Filippidis <jfilippidis@gmail.com>
2021-09-07 15:06:25 +0200

    PEP8: uppercase constants in `setup.py`
    also, in the added lines use formatted string literals,
    instead of `str.format`, since these lines are
    being rewritten.

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ import warnings
 
 
 README = 'README.md'
-name = 'nx2tikz'
-description = 'Export NetworkX graphs to TikZ.'
+NAME = 'nx2tikz'
+DESCRIPTION = 'Export NetworkX graphs to TikZ.'
 try:
     with open('README.md') as f:
         long_description = f.read()
 except FileNotFoundError:
     long_description = ''
     warnings.warn('Could not find {readme}'.format(readme=README))
-url = 'https://github.com/johnyf/{name}'.format(name=name)
-VERSION_FILE = '{name}/_version.py'.format(name=name)
+url = f'https://github.com/johnyf/{NAME}'
+VERSION_FILE = f'{NAME}/_version.py'
 MAJOR = 0
 MINOR = 2
 MICRO = 0
@@ -23,8 +23,8 @@ VERSION_TEXT = (
     '# This file was generated from setup.py\n'
     "version = '{version}'\n")
 PYTHON_REQUIRES = '>=3.9'
-install_requires = ['networkx']
-classifiers = [
+INSTALL_REQUIRES = ['networkx']
+CLASSIFIERS = [
     'Development Status :: 2 - Pre-Alpha',
     'Intended Audience :: Developers',
     'Intended Audience :: Science/Research',
@@ -43,25 +43,25 @@ def run_setup():
     with open(VERSION_FILE, 'w') as f:
         f.write(s)
     setup(
-        name=name,
+        name=NAME,
         version=VERSION,
         license='BSD',
-        description=description,
+        description=DESCRIPTION,
         long_description=long_description,
         author='Ioannis Filippidis',
         author_email='jfilippidis@gmail.com',
         url=url,
         python_requires=PYTHON_REQUIRES,
-        install_requires=install_requires,
-        packages=[name],
-        package_dir={name: name},
+        install_requires=INSTALL_REQUIRES,
+        packages=[NAME],
+        package_dir={NAME: NAME},
         entry_points={
             'console_scripts':
                 ['nx2tikz = nx2tikz.nx2tikz:command_line']},
         keywords=[
             'tikz', 'pgf', 'networkx', 'tex', 'latex', 'luatex',
             'lualatex', 'graph'],
-        classifiers=classifiers)
+        classifiers=CLASSIFIERS)
 
 
 if __name__ == '__main__':
